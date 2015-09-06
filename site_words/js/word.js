e3bb815cb5543e9323c11ea0d82194185cc89043
Bill Tran <bjtran1234@gmail.com>
2015-09-05 21:20:26 -0700

    added site words

---
+++ site_words/js/word.js
@@ -0,0 +1,285 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+$(document).foundation();
+// var tag = document.createElement('script'),
+//     player;
+
+// var wordPicked = ['see','my','to','and','go'];
+// var json_str = JSON.stringify(wordPicked);
+// createCookie('mycookie', json_str);
+
+var listOfWords = ['see','my','to','and','go','is','said','the','for','play','can','you','me','it','big','in','we','not','down','where','little','help','like','have','she','are','was','he','they','with','on','this','at','what','do','no','want','who','but','a','up','his','him','had','yes','baby','banana','car','cat','chimpanzee','clap','diaper','dog','milk','rhinoceros','spider'],
+	wordIndex = 0,
+	videoIndex = 0,
+	playerButton = $('span.player a'),
+	level = 0,
+	searchWords = ['full time kid','frozen','pokemon','minecraft','doggy','lego','barbie','hello kitty','digimon','kitty','yo gabba gabba!','sesame street','fairies','teeanage mutant ninja turtles','how to train a dragon','monster high','tinker bell','octonauts','kid snippets','kid science','monsters ink','stawberry shortcake','winnie the pooh','polly pocket', 'disney', 'princess'],
+	searchFor = '';
+
+shuffle(listOfWords);
+
+function resetPlayer(){
+		$('#gameScreen').show();
+		$('.sound-play').show()
+		$('.player').hide();
+		$('#currentWord p').show();
+		$('#underlineText').show();
+		$('#keyboard').show();
+		playerButton.css('z-index', '-1');
+};
+
+function createSearchWordButton(){
+	var rows = 1; //here's your number of rows and columns
+      var cols = searchWords.length;
+      var table = $('<table><tbody>');
+      for(var r = 0; r < rows; r++){
+        var tr = $('<tr>');
+        for (var c = 0; c < cols; c++)
+          $('<td></td>').appendTo(tr); //fill in your cells with something meaningful here
+        tr.appendTo(table);
+      }
+
+      table.appendTo('.searchScreen section');
+      $('.searchScreen td').each(function(index){
+        $(this).append(searchWords[index])
+      })
+      $('table').addClass('medium-12 columns');
+      $('td').addClass('button round');
+      $('tr').addClass('medium-12 medium-centered');
+      $('tbody').css('text-align','center');
+      $('td').css('margin','5px').css('font-size','50px');
+      // add click event for selected word
+      searchWordClick();
+};
+
+function searchWordClick(){
+	$('table td').on('click', function(){
+		searchFor = toQueryString($(this).text());
+		$('.searchScreen table').remove();
+		searchForVideos();
+	});
+}
+
+function relatedVideos(listOfVideos){
+	$.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId='+listOfVideos[Math.floor(Math.random()*25)+1].videoId+'&safeSearch=strict&type=video&maxResults=50&videoDuration=short&key=AIzaSyDw_LwtiRuFZSpqSMDRn7Oo9K0Ch2SIVnI',
+    dataType: "json",
+    success:  function(data) {
+      var youtubeJsonList = data.items
+      youtubeJsonList.forEach(function(data){
+       videos.push({videoId:data.id.videoId, thumbnail:data.snippet.thumbnails.default.url});
+    	})
+
+    	videos = shuffle(videos);
+    	// function for creating a row and column
+    var rows = 12; //here's your number of rows and columns
+    var cols = 5;
+    var table = $('<table><tbody>');
+    for(var r = 0; r < rows; r++){
+      var tr = $('<tr>');
+      for (var c = 0; c < cols; c++)
+        $('<td></td>').appendTo(tr); //fill in your cells with something meaningful here
+      tr.appendTo(table);
+    }
+
+    table.appendTo('section.player');
+    $('.player td').each(function(index){
+      // $(this).append('<a href="#"></a><div id="videoPlayer'+index+'"></div>')
+      $(this).append('<a href="#"></a><img class="video-image" src='+videos[index].thumbnail+'></div>')
+    })
+    
+    // onYouTubeIframeAPIReady();
+    $('.gameScreen').hide();
+    chosenVideoToPlay();
+    }
+  });
+};
+
+function searchForVideos(){
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search?part=snippet%20&q='+searchFor+'%20&type=video&videoDuration=short&maxResults=50&safeSearch=strict&key=AIzaSyDw_LwtiRuFZSpqSMDRn7Oo9K0Ch2SIVnI',
+    dataType: "json",
+    success:  function(data) {
+      var youtubeJsonList = data.items
+      youtubeJsonList.forEach(function(data, i){
+       videos.push({videoId:data.id.videoId, thumbnail:data.snippet.thumbnails.default.url});
+    	})
+
+    	videos = shuffle(videos);
+
+    	relatedVideos(videos, searchFor)
+    }
+  });
+};
+
+function toQueryString(str){
+	string = str.replace(/\s+/g, '+').toLowerCase();
+	return string
+};
+
+function fullscreen(){
+	var el = document.documentElement, 
+	rfs = el.webkitRequestFullScreen;
+    rfs.call(el);
+}
+
+$('.sound-play').on('click', function(){
+	// fullscreen();
+	$('.sound-play').hide()
+	document.getElementById('whole-word-sound').play();
+	document.getElementById('whole-word-sound').play();
+	setTimeout(function(){
+		$('#currentWord').show()
+	},500);
+});
+
+function displayWordChosen(){
+	$('#currentWord p').empty();
+	$('#matchFullWord li a').empty()
+	$('#underlineText p').empty();
+	chosenWord = listOfWords[wordIndex].toUpperCase()
+			for(i=0; i<chosenWord.length; i++){
+				$('#currentWord p').append(chosenWord[i]);
+				$('#underlineText p').append("_ ");
+			};
+			var chosenWordLowercase = chosenWord.toLowerCase();
+			$('#whole-word-sound').attr('src', 'audio/'+chosenWordLowercase+'.mp4');
+			
+};
+displayWordChosen();
+var matchingLetter = 0,
+	wrongLetter = 0
+
+$('#keyboard li').on('click', function(e){
+	e.preventDefault();
+	var button = $(this).text()
+	$('#inputText p').append(button)
+	if(button === chosenWord[matchingLetter]){
+		matchingLetter = ++matchingLetter
+		wrongLetter = 0
+	}else{
+		setTimeout(function(){
+			var correctLetters = $('#inputText p').text().slice(0,-1)
+			    $('#inputText p').remove()
+			    $('#inputText').append('<p>'+correctLetters+'</p>')
+		},500);
+		if(level === 3){
+			document.getElementById('whole-word-sound').play();
+			wrongLetter = ++wrongLetter
+			console.log('wrong letter '+wrongLetter)
+			if (wrongLetter === 4){
+				$('#currentWord').css('visibility','visible');
+				level = 0
+				wrongLetter = 0
+			}
+		}
+	}
+		console.log('original= '+chosenWord)
+	if(matchingLetter === chosenWord.length){
+		setTimeout(function(){
+			$('#inputText p').remove()
+			$('#inputText').append('<p></p>')
+			$('#currentWord').hide();
+			$('#underlineText').hide();
+			matchingLetter = 0
+			if(level === 3){
+				createSearchWordButton();
+				$('#gameScreen').show();
+				$('.player').show();
+				$('#keyboard').hide();
+				setTimeout(function(){
+					displayWordChosen();
+				},3000);
+				$('#currentWord p').hide();
+				$('#matchFullWord').hide();
+				$( "#matchFullWord li" ).unbind( "click");
+				level = 0
+				wordIndex = ++wordIndex;
+				$('#currentWord').css('visibility','visible');
+			}else{
+				level = 2
+				level2()
+			}
+		},1000);
+		if(wordIndex === (listOfWords.length-1)){
+			wordIndex = 0
+		};
+	};
+})
+
+function level2(){
+	console.log(listOfWords)
+	console.log(wordIndex)
+    var	l2ListOfWords = [],
+    	wordIndexPlus = wordIndex+4
+    	console.log(wordIndexPlus)
+	for(i = wordIndex; i < wordIndexPlus; i++){
+		l2ListOfWords.push(listOfWords[i])
+	};
+	console.log(l2ListOfWords)
+	shuffle(l2ListOfWords);
+	$('#matchFullWord li:nth-child(1) a').append(l2ListOfWords[0])
+	$('#matchFullWord li:nth-child(2) a').append(l2ListOfWords[1])
+	$('#matchFullWord li:nth-child(3) a').append(l2ListOfWords[2])
+	$('#matchFullWord li:nth-child(4) a').append(l2ListOfWords[3])
+	$('#matchFullWord').show();
+	$('#keyboard').hide();
+	setTimeout(function(){
+		document.getElementById('whole-word-sound').play();
+	}, 500)
+	$( "#matchFullWord li" ).bind( "click");
+	$('#matchFullWord li').on('click', function(){ 
+		var word = $(this).children().text().toUpperCase();
+			if(word === chosenWord){
+				level = 3
+				level3();
+			}else{
+				shuffle(l2ListOfWords);
+				console.log("wrong = "+l2ListOfWords)
+				$('#keyboard').show();
+				$('#matchFullWord').hide()
+				$('#currentWord').show();
+				$('#underlineText').show();
+				$('#matchFullWord li a').empty();
+				document.getElementById('whole-word-sound').play();
+				$( "#matchFullWord li" ).unbind( "click");
+			};
+		});
+};
+
+function level3(){
+	console.log('level3')
+	$('#keyboard').show();
+	$('#underlineText').show()
+	$('#currentWord p').show()
+	$('#matchFullWord').hide();
+	$('#matchFullWord li a').empty()
+	$( "#matchFullWord li" ).unbind( "click");
+	document.getElementById('whole-word-sound').play();
+	$('#currentWord').css('visibility', 'hidden').show();
+}
+
+//shuffle function for shuffling the words
+function shuffle(array) {
+    var currentIndex = array.length
+        , temporaryValue
+        , randomIndex
+        ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+};
+
+
+audio = new Audio('audio/big.mp4')
---
+++ site_words/js/youtube.js
@@ -0,0 +1,137 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var videos = [];
+var players;
+var videoThumbnails = [];
+
+function youtubeSetup(i){
+  function onYouTubeIframeAPIReady(i) {
+    console.log('creating youtube')
+    players = new YT.Player('videoPlayer', {
+      height: '390',
+      width: '640',
+      videoId: videos[i].videoId,
+      events: {
+       'onReady': onPlayerReady,
+               // 'onStateChange': onPlayerStateChange
+             }
+           });
+  };
+  onYouTubeIframeAPIReady(i);
+  function onPlayerReady(event) {
+    console.log('onplayerready')
+    playPauseVideo();
+  }
+};
+
+function chosenVideoToPlay(){
+  $('.player a').on('click', function() {
+    $(this)
+      .next()
+        $(this).parent().siblings().hide();
+        $(this).parent().children('img').hide();
+        $(this).addClass("fullscreen-pause");
+        console.log("chosen video play "+$(this).parent().children('img').attr('src'));
+        var videoIndex = $(this).parent().children('img').attr('src');
+        var videoImages = videos.map(function(video){return video.thumbnail});
+        var index = videoImages.indexOf(videoIndex);
+        $(this).parent().append('<a href="#" class="button radius close">X</a><div id="videoPlayer"></div>')
+        youtubeSetup(index);
+        $(this).parent().addClass("fullscreen");
+        $(this).parent().parent().siblings().hide();
+        $(this).unbind('click');
+        close();
+  });
+};
+var playerState;
+var iframeId
+
+function close(){
+  $('.close').on('click', function(){
+    $('iframe').remove();
+      $('.player table').remove();
+      videos = [];
+      videoThumbnails = [];
+      playerState = undefined;
+      resetPlayer();
+  });
+};
+
+var setTimerSeconds = 10;
+var seconds=0;
+
+function timer(){
+  if(seconds === 0){
+    seconds = setTimerSeconds
+    console.log('seconds = '+seconds)
+  };
+  seconds=seconds-1;
+  console.log('seconds = '+seconds)
+
+  if (seconds <= 0){  
+     console.log('clear');
+     seconds === null;
+     return;
+  }else{
+    setTimeout(timer, 1000);
+  }
+};
+
+function playPauseVideo(){
+  var count = 0
+  if(playerState === undefined){
+    playerState = 'play'
+    var duration = players.getDuration();
+    players.unMute();
+    checkPlayerState();
+  }
+
+  function checkPlayerState(){
+    if(players.getPlayerState() !== 0 ){
+      setTimeout(function(){
+       checkPlayerState();
+       console.log('check');
+        if(players.getPlayerState() === 1 && count === 0){
+          $('.fullscreen-pause').css('z-index', '1')
+          count = 1
+        }
+     },1000)
+    }else{
+      console.log('stopped')
+      $('iframe').remove();
+      $('.player table').remove();
+      videos = [];
+      videoThumbnails = [];
+      playerState = undefined;
+      resetPlayer();
+    }
+  };
+  function play(){
+    $('.fullscreen-pause').bind('click', function(){
+      playerState = 'pause'
+      players.pauseVideo();
+      $(this).unbind('click');
+      console.log('pause')
+      $(this).addClass('fullscreen-play');
+      $(this).removeClass('fullscreen-pause');
+      pause();
+    });
+  };
+
+  function pause(){
+     $('.fullscreen-play').bind('click', function(){
+      playerState = 'play'
+      players.playVideo();
+      $(this).unbind('click');
+      console.log('play')
+      $(this).addClass('fullscreen-pause');
+      $(this).removeClass('fullscreen-play');
+      play();
+    });
+  };
+  play();
+}
